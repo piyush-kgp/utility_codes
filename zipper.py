477013269a3544f16d12e89e286a222ff8416900
piyush-kgp <piyushsingh@iitkgp.ac.in>
2018-01-27 12:22:33 +0530

    Update zipper.py

--- zipper.py
+++ zipper.py
@@ -4,10 +4,12 @@ import zipfile
 #enter working directory here
 os.chdir('./Desktop/auto_vpn')
 dir_name = os.getcwd().split('\\')[len(os.getcwd().split('\\'))-1]
-a=zipfile.ZipFile(dir_name+'.zip', 'w', zipfile.ZIP_DEFLATED)
+#we are creating a zip file whose name is same as that of the directory, it can be changed to whatever..
+ziphandle=zipfile.ZipFile(dir_name+'.zip', 'w', zipfile.ZIP_DEFLATED)
 for root, dirs, files in os.walk(os.getcwd()):
     for file in files: 
         f=os.path.join(root, file)
-        a.write(f)
+        if file==dir_name+'.zip' : continue
+        ziphandle.write(f)
         print(str(f)+' added')
-a.close()
+ziphandle.close()
