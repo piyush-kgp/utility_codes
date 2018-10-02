b344f83da0065630ac8fee04ea8be000bfd8b1ef
piyush-kgp <piyushsinghkgpian@gmail.com>
2018-10-02 16:02:02 +0530

    Update timer.sh

--- timer.sh
+++ timer.sh
@@ -1,13 +1,16 @@
-echo "Countdown time in seconds"
-read secs
-t1=(`date +%s`);
-while true;do
-	t2=(`date +%s`)
-	echo -ne "$((secs-t2+t1))\r"
-	if [ "$((secs-t2+t1))" -le 0 ]
-	then
-		echo "TIME UP"
-		say "Countdown over"
-	fi
-	sleep 0.1
-done
+# Add this to your ~/.bash_profile and then you can say countdown at terminal.
+countdown(){
+	echo "Enter Countdown time in seconds"
+	read secs
+	t1=(`date +%s`);
+	while true;do
+		t2=(`date +%s`)
+		echo -ne "$((secs-t2+t1))\r"
+		if [ "$((secs-t2+t1))" -le 0 ]
+		then
+			echo "TIME UP"
+			say "Your $secs seconds are over. Countdown is over"
+		fi
+		sleep 0.1
+	done
+}
